4f6da873ef2bf9d506bfc97d8896082680f33af7
ryb <1298166988@qq.com>
2024-06-29 12:34:11 +0800

    引入前端

--- gulimall-product/src/main/java/com/ryb/gulimall/product/controller/CategoryController.java
+++ gulimall-product/src/main/java/com/ryb/gulimall/product/controller/CategoryController.java
@@ -1,6 +1,7 @@
 package com.ryb.gulimall.product.controller;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 
@@ -32,14 +33,12 @@ public class CategoryController {
     private CategoryService categoryService;
 
     /**
-     * 列表
+     * 查出所有分类以及子类，以树形结构组装起来
      */
-    @RequestMapping("/list")
-    //@RequiresPermissions("product:category:list")
-    public R list(@RequestParam Map<String, Object> params){
-        PageUtils page = categoryService.queryPage(params);
-
-        return R.ok().put("page", page);
+    @RequestMapping("/list/tree")
+    public R list(){
+        List<CategoryEntity> list = categoryService.listWithTree();
+        return R.ok().put("data", list);
     }
 
 
@@ -82,7 +81,9 @@ public class CategoryController {
     @RequestMapping("/delete")
     //@RequiresPermissions("product:category:delete")
     public R delete(@RequestBody Long[] catIds){
-		categoryService.removeByIds(Arrays.asList(catIds));
+		//categoryService.removeByIds(Arrays.asList(catIds));
+
+        categoryService.removeMenuByIds(Arrays.asList(catIds));
 
         return R.ok();
     }
--- gulimall-product/src/main/java/com/ryb/gulimall/product/service/impl/CategoryServiceImpl.java
+++ gulimall-product/src/main/java/com/ryb/gulimall/product/service/impl/CategoryServiceImpl.java
@@ -1,7 +1,12 @@
 package com.ryb.gulimall.product.service.impl;
 
 import org.springframework.stereotype.Service;
+
+import java.util.Comparator;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -24,6 +29,42 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
         );
 
         return new PageUtils(page);
+    }
+
+    @Override
+    public List<CategoryEntity> listWithTree() {
+        List<CategoryEntity> entities = baseMapper.selectList(null);
+        List<CategoryEntity> level1Menus = entities.stream().filter(categoryEntity -> categoryEntity.getParentCid() == 0)
+                .map((menu)->{
+                    menu.setChildren(getChildren(menu,entities));
+                    return menu;
+                })
+                .sorted((menu1, menu2) -> {
+                    return (menu1.getSort() ==null?0:menu1.getSort()) - (menu2.getSort() ==null?0:menu2.getSort());
+                })
+                .collect(Collectors.toList());
+
+        return level1Menus;
+    }
+
+    @Override
+    public void removeMenuByIds(List<Long> list) {
+
+        baseMapper.deleteBatchIds(list);
+    }
+
+
+    private List<CategoryEntity> getChildren(CategoryEntity root, List<CategoryEntity> all) {
+        List<CategoryEntity> children = all.stream().filter(categoryEntity -> categoryEntity.getParentCid().equals(root.getCatId()))
+                .map(categoryEntity -> {
+                    categoryEntity.setChildren(getChildren(categoryEntity, all));
+                    return categoryEntity;
+                })
+                .sorted((menu1, menu2) -> {
+                    return (menu1.getSort() ==null?0:menu1.getSort()) - (menu2.getSort() ==null?0:menu2.getSort());
+                })
+                .collect(Collectors.toList());
+        return children;
     }
 
 }
